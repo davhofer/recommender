dfdf426029362a9e3289f6c0c2c322202e8bd3dd
David Hofer <davhofer@ethz.ch>
2023-04-25 11:17:03 +0200

    refactor code, move model and preprocessing to separate files

---
+++ data.py
@@ -0,0 +1,69 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import random
+import pandas as pd
+import numpy as np
+
+def preprocess_transactions(df):
+        
+    df = df[~df['topic_id'].isna()]
+    df['topic_id'] = df['topic_id'].astype(int)
+
+    # only keep users with at least 10 interactions
+    df = df.groupby('user_id').filter(lambda x: len(x) >= 10)
+
+    # all user_ids in the dataset
+    user_ids = list(df['user_id'].unique())
+
+    # all topic_ids in the dataset
+    topic_ids = list(df['topic_id'].unique())
+
+    # train test split
+    df['start_time'] = pd.to_datetime(df['start_time'])
+
+    cut = np.percentile(df['start_time'], 80)
+
+    train_df = df[df['start_time'] < cut]
+    test_df = df[df['start_time'] >= cut]
+
+    return train_df, test_df, user_ids, topic_ids
+
+
+
+
+class TransactionsStudentsTopicsDS(Dataset):
+    def __init__(self, df, user_ids, topic_ids, negative_frac=1.0):
+
+        self.user_ids = user_ids
+        self.topic_ids = topic_ids
+
+     
+        interactions = list(df.groupby(['user_id', 'topic_id']).count().index)
+
+        all_pairings = {(user, topic) for user in user_ids for topic in topic_ids}
+        positives = set(interactions)
+        no_interaction = all_pairings - positives
+        negatives = random.sample(list(no_interaction), int(negative_frac*len(positives)))
+
+        self.data = [(x[0], x[1], 1.0) for x in positives] + [(x[0], x[1], 0.0) for x in negatives]
+        
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        user, topic, y = self.data[index]
+
+        user = self.user_ids.index(user)
+        user = torch.tensor(user)
+
+        topic = self.topic_ids.index(topic)
+        topic = torch.tensor(topic)
+
+        y = torch.tensor([y])
+        return user, topic, y
+
+
+def get_transactions_dataloader(dataframe, batch_size, negative_frac=1.0):
+    dataset = TransactionsStudentsTopicsDS(dataframe, negative_frac=negative_frac)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=True)
---
+++ ncf_model.py
@@ -0,0 +1,61 @@
+import torch
+from torch import optim, nn
+import lightning.pytorch as pl
+
+class NCFNetwork(pl.LightningModule):
+    def __init__(self, num_students, num_topics, student_embedding_dim, topic_embedding_dim, loss=nn.BCELoss()):
+        super().__init__()
+        self.student_embedding_layer = nn.Embedding(num_students, student_embedding_dim)
+        self.topic_embedding_layer = nn.Embedding(num_topics, topic_embedding_dim)
+        
+        self.network = nn.Sequential(
+            nn.Linear(student_embedding_dim+topic_embedding_dim, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 1),
+            nn.Sigmoid(),
+        )
+
+        self.loss = loss
+
+        self.predict_proba = torch.Tensor()
+
+        self.save_hyperparameters()
+
+    def forward(self, student_x, topic_x):
+        student_emb = self.student_embedding_layer(student_x)
+        topic_emb = self.topic_embedding_layer(topic_x)
+
+        x = torch.cat((student_emb, topic_emb), 1)
+
+        proba = self.network(x)
+
+        return proba
+
+
+    def training_step(self, batch, batch_idx):
+        student_x, topic_x, y = batch
+
+        y_proba = self(student_x, topic_x)
+
+        loss = self.loss(y_proba, y)
+        self.log("train_loss", loss)
+
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        student_x, topic_x, y = batch
+
+        y_proba = self(student_x, topic_x)
+
+        self.predict_proba = torch.cat((self.predict_proba, y_proba.detach().cpu()))
+
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+

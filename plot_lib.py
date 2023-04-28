55fbf6164a97e5779ba219519bb4b4955ee02822
tranhuonglan <91091369+tranhuonglan@users.noreply.github.com>
2023-04-28 21:44:35 +0200

    final fix for plots

--- plot_lib.py
+++ plot_lib.py
@@ -1,16 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def iteration_error_plot(model_error_dict):
     """
     @args: 
     model_error_dict: dict {'model_name': [model_error_list, iteration]} indicates training error according to each epoch
     """
 
+    # Compute the smoothed values of the error using a rolling mean
+
+    batch_size = 64
+
     for model_name, error_log in model_error_dict.items():
-        error_list, iteration = error_log[0], error_log[1]
+        error_list, iteration = np.array(error_log[0]), np.array(error_log[1])
+        for i in range(0, error_list.shape[0], batch_size):
+            error_list[i: (i + batch_size)] = np.mean(error_list[i: (i + batch_size)] )        
+
+        # Replace the original array with the means array
         plt.plot(iteration, error_list, label=model_name)
+       
 
     # add labels and title
     plt.xlabel('Number of Iterations')
@@ -28,14 +36,16 @@ def predictive_factor_eval_plot(factor_arr, hr_dict, ndcg_dict, n):
     hr_dict: dict {'model_name': hr_score} of hit rate score according to factor
     ndcg_dict: dict {'model_name': ndcg_score} of ndcg_arr score according to factor
     """
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (8, 4))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 4))
     factor_arr = np.array(factor_arr).astype(str)
 
     for model_name, hr_score in hr_dict.items():
         ax1.plot(factor_arr, hr_score, label=model_name)
+        ax1.scatter(factor_arr, hr_score)
 
     for model_name, ndcg_score in ndcg_dict.items():
         ax2.plot(factor_arr, ndcg_score, label=model_name)
+        ax2.scatter(factor_arr, ndcg_score)
 
     # add labels and title
     ax1.set_xlabel('Number of Predictive Factors')
@@ -56,14 +66,16 @@ def k_eval_plot(k_arr, hr_dict, ndcg_dict, n=10):
     hr_dict: dict {'model_name': hr_score} of hit rate score according to factor
     ndcg_dict: dict {'model_name': ndcg_score} of ndcg_arr score according to factor
     """
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (8, 4))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 4))
     
 
     for model_name, hr_score in hr_dict.items():
         ax1.plot(k_arr, hr_score, label=model_name)
+        ax1.scatter(k_arr, hr_score)
 
     for model_name, ndcg_score in ndcg_dict.items():
         ax2.plot(k_arr, ndcg_score, label=model_name)
+        ax2.scatter(k_arr, ndcg_score)
 
     # add labels and title
     ax1.set_xlabel('K')

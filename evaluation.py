fixed evaluation & metric computation bug, updated data pipeline for verbosity and (some)  efficiency

evaluation.py
@@ -2,24 +2,14 @@ import math
 import numpy as np
 import pandas as pd
 
-def HitRate_NDCG_MRR(proba_all_topic_csv, n=10):
-    """
-    @args:
-    proba_all_topic_csv: {user_id, topic_id, was_interaction, predict_proba} csv file return recommendation probability for every topic 
-    n: keep top n topic recommendation
-
-    @output:
-    hit_list: (array) hit[i] == 0/1 indicates hit/miss for test sample i 
-    ndcg_list: (array) ndcg[i] score for test sample ith
-    """
+def HitRate_NDCG_MRR(df, n):
     hit_list = []
     ndcg_list = []
     mrr_list = []
-    proba_all_topic_df = pd.read_csv(proba_all_topic_csv)
-    user_predict = proba_all_topic_df.groupby(['user_id'])
+
+    user_predict = df.groupby(['user_id'])
 
     for user, topic in user_predict:
-        #print(topic)
         # Get the top N of highest probability and rank them 
         topN = [x for _, x in sorted(zip(topic['predict_proba'], topic['topic_id']), reverse=True)][:n]
         positive_topic = int(topic[topic['was_interaction']==1]['topic_id'])
@@ -29,31 +19,55 @@ def HitRate_NDCG_MRR(proba_all_topic_csv, n=10):
         # Calculate NDCG
         ndcg_list.append(getNDCG(topN, positive_topic))
 
-        #Calculate MRR
+        # Calculate MRR
         mrr_list.append(getMRR(topN, positive_topic))
         
-    return np.array(hit_list).mean(), np.array(ndcg_list).mean(), np.array(mrr_list).mean()
+    return {f'HitRate@{n}': np.array(hit_list).mean(), f'NDCG@{n}': np.array(ndcg_list).mean(), f'MRR@{n}': np.array(mrr_list).mean()}
 
+
+
+
+def HitRate_NDCG_MRR_from_CSV(proba_all_topic_csv, n=10):
+    """
+    @args:
+    proba_all_topic_csv: {user_id, topic_id, was_interaction, predict_proba} csv file return recommendation probability for every topic 
+    n: keep top n topic recommendation
+
+    @output:
+    hit_list: (array) hit[i] == 0/1 indicates hit/miss for test sample i 
+    ndcg_list: (array) ndcg[i] score for test sample ith
+    """
+
+    proba_all_topic_df = pd.read_csv(proba_all_topic_csv)
+    metrics = compute_metrics(proba_all_topic_df, n)
+    return metrics
 
 def getMRR(ranklist, topic):
-    for i in range(len(ranklist)):
-        if ranklist[i] == topic:
-            return 1/i
-    return 0
+    if topic not in ranklist:
+        return 0
+    return 1.0/(ranklist.index(topic) + 1)
+    # for i in range(len(ranklist)):
+    #     if ranklist[i] == topic:
+    #         return 1/i
+    # return 0
 
 def getHitRatio(ranklist, topic):
-    for item in ranklist:
-        if item == topic:
-            return 1
-    return 0
+    return int(topic in ranklist)
+    # for item in ranklist:
+    #     if item == topic:
+    #         return 1
+    # return 0
 
 def getNDCG(ranklist, topic):
-    for i in range(len(ranklist)):
-        if ranklist[i] == topic:
-            return math.log(2) / math.log(i+2)
-    return 0
+    if topic not in ranklist:
+        return 0
+    return math.log(2) / math.log(ranklist.index(topic)+2)
+    # for i in range(len(ranklist)):
+    #     if ranklist[i] == topic:
+    #         return math.log(2) / math.log(i+2)
+    # return 0
 
 
 if __name__ == '__main__':
-    HitRate_NDCG_MRR('ncf_64_predictive_factors_first_try_outputs.csv', 5)
+    HitRate_NDCG_MRR_from_CSV('ncf_64_predictive_factors_first_try_outputs.csv', 5)
     

ncf_model.py
@@ -4,6 +4,7 @@ import pytorch_lightning as pl
 import pandas as pd
 import numpy as np
 import math
+from evaluation import HitRate_NDCG_MRR
 
 
 def get_MLP(in_size, out_size, num_layers, softmax_out=False, dimension_decay='linear'):
@@ -195,33 +196,6 @@ class NCFNetwork(pl.LightningModule):
     
     
     def compute_metrics(self, k):
-        def getHitRatio(ranklist, topic):
-            return int(topic in ranklist)
-            
-        def getNDCG(ranklist, topic):
-            if topic not in ranklist:
-                return 0
-            return math.log(2) / math.log(ranklist.index(topic)+2)
-            
-
-        hit_list = []
-        ndcg_list = []
-
         eval_results = self.eval_results
         df = pd.DataFrame({'user_id': eval_results[0], 'topic_id': eval_results[1], 'was_interaction': eval_results[2].flatten(), 'predict_proba': eval_results[3].flatten()})
-
-        user_predict = df.groupby(['user_id'])
-
-        for user, topic in user_predict:
-            # Get the top N of highest probability and rank them 
-            topN = [x for _, x in sorted(zip(topic['predict_proba'], topic['topic_id']), reverse=True)][:k]
-            positive_topic = int(topic[topic['was_interaction']==1]['topic_id'])
-            # Calculate hit rate
-            hit_list.append(getHitRatio(topN, positive_topic))
-            
-            # Calculate NDCG
-            ndcg_list.append(getNDCG(topN, positive_topic))
-            
-        return {f'HitRate@{k}': np.array(hit_list).mean(), f'NDCG@{k}': np.array(ndcg_list).mean()}
-
-
+        return HitRate_NDCG_MRR(df, k)